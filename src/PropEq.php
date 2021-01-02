ffcdb7a413f5afb6c5d6d1dbeb1862c9b0589800
Anthea <Anthea@Anthea-PC.broadband>
2021-01-02 18:52:32 +0000

    Added propEq function

---
+++ src/PropEq.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * (c) Matthew Taylor
+ */
+
+namespace RamdaPHP;
+
+trait PropEq 
+{
+    public static function propEq(...$args)
+    {
+        $propEq = self::curry(function($propName, $val, $target) {
+            $v = self::prop($propName, $target);
+            return self::equals($v, $val);
+        });
+        return $propEq(...$args);
+    }
+}
