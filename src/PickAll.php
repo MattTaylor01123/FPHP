4e81c773aff75815dbd347f1f58ad2261780762b
Matt <MattTaylor012333@git.hub>
2021-02-21 20:31:24 +0000

    Added test case for pickAll, corrected references to other lib funcs, and corrected call to non-existent function.

--- src/PickAll.php
+++ src/PickAll.php
@@ -6,13 +6,15 @@
 
 namespace RamdaPHP;
 
+use RamdaPHP\RamdaPHP as R;
+
 trait PickAll
 {
     public static function pickAll(...$args)
     {
         // TODO - what about items that are missing?
         $pickAll = R::curry(function(iterable $props, $target) {
-            return R::filter(fn($v, $k) => R::contains($k, $props), $target);
+            return R::filter(fn($v, $k) => R::includes($k, $props), $target);
         });
         return $pickAll(...$args);
     }
