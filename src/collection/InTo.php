2ae3d8cc95bb6f5407858e752ff3c5708681e900
Matt <MattTaylor012333@git.hub>
2021-03-12 23:19:17 +0000

    Minor refactor.

--- src/collection/InTo.php
+++ src/collection/InTo.php
@@ -11,10 +11,9 @@ trait InTo
     public static function inTo(...$args)
     {
         $inTo = self::curry(function($initial, callable $transducer, $collection) {
-            $transInto = self::transduce($transducer, self::__(), $initial, $collection);
             if(is_object($initial) || is_array($initial))
             {
-                return $transInto(self::assoc());
+                return self::transduce($transducer, self::assoc(), $initial, $collection);
             }
             else
             {
