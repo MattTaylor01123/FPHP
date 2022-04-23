014e89e6d8466a737633cef077edc2fe7ef2c29a
Matt <MattTaylor012333@git.hub>
2022-04-23 18:38:26 +0100

    Added an "intoAssoc" function. The origianl "into" now always uses "append" as the step, whereas "intoAssoc" uses "assoc" as the step.

--- src/collection/InTo.php
+++ src/collection/InTo.php
@@ -11,22 +11,15 @@ trait InTo
     public static function inTo(...$args)
     {
         $inTo = self::curry(function($initial, callable $transducer, $collection) {
-            if(is_object($initial))
-            {
-                return self::transduce($transducer, self::assoc(), $initial, $collection);
-            }
-            // if the target is an array then assume we need a non-associative (classic)
-            // array. If associative array is required then have to use transduce.
-            elseif(is_array($initial))
-            {
-                return self::transduce($transducer, self::append(), $initial, $collection);
-            }
-            else
-            {
-                throw new InvalidArgumentException(
-                    "Invalid type for input 'collection'"
-                );
-            }
+            return self::transduce($transducer, self::append(), $initial, $collection);
+        });
+        return $inTo(...$args);
+    }
+
+    public static function inToAssoc(...$args)
+    {
+        $inTo = self::curry(function($initial, callable $transducer, $collection) {
+            return self::transduce($transducer, self::assoc(), $initial, $collection);
         });
         return $inTo(...$args);
     }
--- tests/collection/InToTest.php
+++ tests/collection/InToTest.php
@@ -23,7 +23,7 @@ final class InToTest extends TestCase
             F::filterT(fn($v) => $v % 2)
         );
 
-        $out1 = F::inTo(new stdClass(), $fn, $this->getAssocArray());
+        $out1 = F::inToAssoc(new stdClass(), $fn, $this->getAssocArray());
         $out2 = F::inTo([], $fn, $this->getAssocArray());
         
         $this->assertEquals((object)["a" => 2, "c" => 4, "e" => 6], $out1);
@@ -35,7 +35,7 @@ final class InToTest extends TestCase
         $v = new TestType();
         $v->a = 1;
         $v->b = "h";
-        $o = F::inTo(new TestType(), F::adjustT("a", F::inc()), $v);
+        $o = F::inToAssoc(new TestType(), F::adjustT("a", F::inc()), $v);
 
         $exp = new TestType();
         $exp->a = 2;
