30fb7e4225fcd58dfd8b5bfa388fa34013e80ae6
Matt <MattTaylor012333@git.hub>
2022-04-23 14:40:17 +0100

    Updated "prop" to support deferral of calls to targets with "prop" or "get" functions.

--- src/collection/Prop.php
+++ src/collection/Prop.php
@@ -11,17 +11,37 @@ trait Prop
     public static function prop(...$args)
     {
         $prop = self::curry(function($propName, $target) {
-            if(is_object($target))
-            {
-                $out = $target->$propName ?? null;
-            }
-            else if(is_array($target))
+            if(is_array($target))
             {
                 $out = $target[$propName] ?? null;
             }
-            else
+            else if(is_object($target) && method_exists($target, "prop"))
+            {
+                $out = $target->prop($propName);
+            }
+            else if(is_object($target) && method_exists($target, "get"))
+            {
+                $out = $target->get($propName);
+            }
+            else if($target instanceof \Traversable)
             {
                 $out = null;
+                foreach($target as $k => $v)
+                {
+                    if($k === $propName)
+                    {
+                        $out = $v;
+                        break;
+                    }
+                }
+            }
+            else if(is_object($target))
+            {
+                $out = $target->$propName ?? null;
+            }
+            else
+            {
+                throw new InvalidArgumentException("Invalid type for 'target'");
             }
             return $out;
         });
