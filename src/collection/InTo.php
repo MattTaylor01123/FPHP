0e0daf84f6a27a2fc96e3134194a20a9ab5471e4
Matt <MattTaylor012333@git.hub>
2022-08-20 23:21:39 +0100

    Removed currying from "inTo"

--- src/collection/InTo.php
+++ src/collection/InTo.php
@@ -8,19 +8,13 @@ namespace FPHP\collection;
 
 trait InTo
 {
-    public static function inTo(...$args)
+    public static function inTo($initial, callable $transducer, $collection)
     {
-        $inTo = self::curry(function($initial, callable $transducer, $collection) {
-            return self::transduce($transducer, fn($acc, $v) => self::append($acc, $v), $initial, $collection);
-        });
-        return $inTo(...$args);
+        return self::transduce($transducer, fn($acc, $v) => self::append($acc, $v), $initial, $collection);
     }
 
-    public static function inToAssoc(...$args)
+    public static function inToAssoc($initial, callable $transducer, $collection)
     {
-        $inTo = self::curry(function($initial, callable $transducer, $collection) {
-            return self::transduce($transducer, fn($acc, $v, $k) => self::assoc($acc, $v, $k), $initial, $collection);
-        });
-        return $inTo(...$args);
+        return self::transduce($transducer, fn($acc, $v, $k) => self::assoc($acc, $v, $k), $initial, $collection);
     }
 }
