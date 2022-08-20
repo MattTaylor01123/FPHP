61e21f36122b08b21755509f5724f5091a517b79
Matt <MattTaylor012333@git.hub>
2022-08-20 23:51:02 +0100

    Removed currying from "partition"

--- src/collection/Partition.php
+++ src/collection/Partition.php
@@ -8,75 +8,82 @@ namespace FPHP\collection;
 
 trait Partition
 {
-    public static function partitionByT(...$args)
+    public static function partitionByT(callable $fnGroup, callable $step)
     {
-        $partitionByT = self::partitionReduceByT(self::__(), self::append(), [], self::__());
-        return $partitionByT(...$args);
+        return self::partitionReduceByT(
+            $fnGroup,
+            fn($acc, $v) => self::append($acc, $v),
+            [],
+            $step
+        );
     }
 
-    public static function partitionMapByT(...$args)
+    public static function partitionMapByT(callable $fnGroup, callable $fnMap, callable $step)
     {
-        $partitionMapByT = self::curry(function(callable $fnGroup, callable $fnMap, callable $step) {
-            return self::partitionReduceByT($fnGroup, function($acc, $v, $k) use($fnMap) {
-                return self::append($acc, $fnMap($v, $k));
-            }, [], $step);
-        });
-        return $partitionMapByT(...$args);
+        return self::partitionReduceByT(
+            $fnGroup,
+            fn($acc, $v, $k) => self::append($acc, $fnMap($v, $k)),
+            [],
+            $step
+        );
     }
 
-    public static function partitionReduceByT(...$args)
+    public static function partitionReduceByT(callable $fnGroup, callable $fnReduce, $initial, callable $step)
     {
-        $partitionReduceByT = self::curry(function(callable $fnGroup, callable $fnReduce, $initial, callable $step) {
-            $started = false;
-            $grp = null;
-            $cache = null;
-            return function ($acc, $v, $k) use($fnGroup, $step, &$grp, &$cache, &$started, $fnReduce, $initial) {
-                $currGrp = $fnGroup($v, $k);
-                if(!$started)
-                {
-                    $started = true;
-                    $grp = $currGrp;
-                    $cache = $fnReduce(self::emptied($initial), $v, $k);
-                    $out = $acc;
-                }
-                else if($currGrp !== $grp)
-                {
-                    $out = $step($acc, $cache, $grp);
-                    $cache = $fnReduce(self::emptied($initial), $v, $k);
-                    $grp = $currGrp;
-                }
-                else
-                {
-                    $cache = $fnReduce($cache, $v, $k);
-                    $out = $acc;
-                }
-                return $out;
-            };
-        });
-        return $partitionReduceByT(...$args);
+        $started = false;
+        $grp = null;
+        $cache = null;
+        return function ($acc, $v, $k) use($fnGroup, $step, &$grp, &$cache, &$started, $fnReduce, $initial) {
+            $currGrp = $fnGroup($v, $k);
+            if(!$started)
+            {
+                $started = true;
+                $grp = $currGrp;
+                $cache = $fnReduce(self::emptied($initial), $v, $k);
+                $out = $acc;
+            }
+            else if($currGrp !== $grp)
+            {
+                $out = $step($acc, $cache, $grp);
+                $cache = $fnReduce(self::emptied($initial), $v, $k);
+                $grp = $currGrp;
+            }
+            else
+            {
+                $cache = $fnReduce($cache, $v, $k);
+                $out = $acc;
+            }
+            return $out;
+        };
     }
 
-    public static function partitionBy(...$args)
+    public static function partitionBy(callable $fnGroup, iterable $target)
     {
-        $partitionBy = self::curry(function(callable $fnGroup, iterable $target) {
-            return self::transduce(self::partitionByT($fnGroup), self::assoc(), self::emptied($target), $target);
-        });
-        return $partitionBy(...$args);
+        return self::transduce(
+            self::partitionByT($fnGroup),
+            fn($acc, $v, $k) => self::assoc($acc, $v, $k),
+            self::emptied($target),
+            $target
+        );
     }
 
-    public static function partitionReduceBy(...$args)
+    public static function partitionReduceBy(callable $fnGroup, callable $fnReducer, $initial, iterable $target)
     {
-        $partitionReduceBy = self::curry(function(callable $fnGroup, iterable $target) {
-            return self::transduce(self::partitionReduceByT($fnGroup), self::assoc(), self::emptied($target), $target);
-        });
-        return $partitionReduceBy(...$args);
+        return self::transduce(
+            fn($step) => self::partitionReduceByT($fnGroup, $fnReducer, $initial, $step),
+            fn($acc, $v, $k) => self::assoc($acc, $v, $k),
+            self::emptied($target),
+            $target
+        );
     }
 
-    public static function partitionMapBy(...$args)
+    public static function partitionMapBy(callable $fnGroup, callable $fnMap, iterable $target)
     {
-        $partitionMapBy = self::curry(function(callable $fnGroup, iterable $target) {
-            return self::transduce(self::partitionMapByT($fnGroup), self::assoc(), self::emptied($target), $target);
-        });
-        return $partitionMapBy(...$args);
+        return self::transduce(
+            fn($step) => self::partitionMapByT($fnGroup, $fnMap, $step),
+            fn($acc, $v, $k) => self::assoc($acc, $v, $k),
+            self::emptied($target),
+            $target
+        );
     }
 }
