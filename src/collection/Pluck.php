44d4b8d56318b29f153568dee028e91d04e5e6f0
Matt <MattTaylor012333@git.hub>
2022-08-20 23:56:41 +0100

    Removed currying from "pluck"

--- src/collection/Pluck.php
+++ src/collection/Pluck.php
@@ -10,29 +10,26 @@ use Traversable;
 
 trait Pluck
 {
-    public static function pluck(...$args)
+    public static function pluck(string $propName, iterable $iterable)
     {
-        $pluck = self::curry(function(string $propName, iterable $iterable) {
-            if(is_object($iterable) && method_exists($iterable, "pluck"))
-            {
-                $out = $iterable->pluck($propName);
-            }
-            else if(is_array($iterable))
-            {
-                $out = array_column($iterable, $propName);
-            }
-            else if($iterable instanceof Traversable)
-            {
-                $out = self::map(self::prop($propName), $iterable);
-            }
-            else
-            {
-                throw new InvalidArgumentException(
-                    "unrecognised iterable"
-                );
-            }
-            return $out;
-        });
-        return $pluck(...$args);
+        if(is_object($iterable) && method_exists($iterable, "pluck"))
+        {
+            $out = $iterable->pluck($propName);
+        }
+        else if(is_array($iterable))
+        {
+            $out = array_column($iterable, $propName);
+        }
+        else if($iterable instanceof Traversable)
+        {
+            $out = self::map(self::prop($propName), $iterable);
+        }
+        else
+        {
+            throw new InvalidArgumentException(
+                "unrecognised iterable"
+            );
+        }
+        return $out;
     }
 }
