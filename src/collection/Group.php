f6122813baf5cc86d954a586c24e678b1403fd33
Matt <MattTaylor012333@git.hub>
2022-08-20 23:17:58 +0100

    Removed currying from "groupBy"

--- src/collection/Group.php
+++ src/collection/Group.php
@@ -8,41 +8,42 @@ namespace FPHP\collection;
 
 trait Group
 {
-    public static function groupBy(...$args)
+    public static function groupBy(callable $fnGroup, iterable $target)
     {
-        $groupBy = self::groupReduceBy(self::__(), fn($acc, $v) => self::append($acc, $v), [], self::__());
-        return $groupBy(...$args);
+        return self::groupReduceBy(
+            $fnGroup,
+            fn($acc, $v) => self::append($acc, $v),
+            [],
+            $target
+        );
     }
 
-    public static function groupMapBy(...$args)
+    public static function groupMapBy(callable $fnGroup, callable $fnMap, iterable $target)
     {
-        $groupMapBy = self::curry(function(callable $fnGroup, callable $fnMap, iterable $target) {
-            return self::groupReduceBy($fnGroup, function($acc, $v, $k) use($fnMap) {
-                return self::append($acc, $fnMap($v, $k));
-            }, [], $target);
-        });
-        return $groupMapBy(...$args);
+        return self::groupReduceBy(
+            $fnGroup,
+            fn($acc, $v, $k) => self::append($acc, $fnMap($v, $k)),
+            [],
+            $target
+        );
     }
 
-    public static function groupReduceBy(...$args)
+    public static function groupReduceBy(callable $fnGroup, callable $fnReduce, $initial, iterable $target)
     {
-        $groupReduceBy = self::curry(function(callable $fnGroup, callable $fnReduce, $initial, iterable $target) {
-            $out = array();
-            foreach($target as $k => $v)
+        $out = array();
+        foreach($target as $k => $v)
+        {
+            $g = $fnGroup($v, $k);
+            if($g === null)
             {
-                $g = $fnGroup($v, $k);
-                if($g === null)
-                {
-                    continue;
-                }
-                if(!self::hasProp($g, $out))
-                {
-                    $out[$g] = self::emptied($initial);
-                }
-                $out[$g] = $fnReduce($out[$g], $v, $k);
+                continue;
             }
-            return $out;
-        });
-        return $groupReduceBy(...$args);
+            if(!self::hasProp($g, $out))
+            {
+                $out[$g] = self::emptied($initial);
+            }
+            $out[$g] = $fnReduce($out[$g], $v, $k);
+        }
+        return $out;
     }
 }
