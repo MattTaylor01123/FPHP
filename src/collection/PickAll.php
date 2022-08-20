6a0eb1ced5ff7877cb2b7772b8cc2db91ad51c3b
Matt <MattTaylor012333@git.hub>
2022-08-20 23:56:08 +0100

    Removed currying from "pickAll"

--- src/collection/PickAll.php
+++ src/collection/PickAll.php
@@ -8,14 +8,9 @@ namespace FPHP\collection;
 
 trait PickAll
 {
-    public static function pickAll(...$args)
+    public static function pickAll(iterable $props, $target)
     {
         // TODO - what about items that are missing?
-        $pickAll = self::curry(function(iterable $props, $target) {
-            return self::filter(function($v, $k) use($props) {
-                return self::includes($k, $props);
-            }, $target);
-        });
-        return $pickAll(...$args);
+        return self::filter(fn($v, $k) => self::includes($k, $props), $target);
     }
 }
