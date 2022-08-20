6799fcb70b48b39893400810d147df55918f2afd
Matt <MattTaylor012333@git.hub>
2022-08-20 23:09:23 +0100

    Removed currying from "emptied"

--- src/collection/Emptied.php
+++ src/collection/Emptied.php
@@ -8,44 +8,41 @@ namespace FPHP\collection;
 
 trait Emptied
 {
-    public static function emptied(...$args)
+    public static function emptied($v)
     {
-        $emptied = self::curry(function($v) {
-            if(is_array($v))
-            {
-                return [];
-            }
-            if(is_iterable($v))
-            {
-                $fn = function() {
-                    yield from [];
-                };
-                $init = self::generatorToIterable($fn);
-                return $init;
-            }
-            if(is_string($v))
-            {
-                return "";
-            }
-            if(is_int($v))
-            {
-                return 0;
-            }
-            if(is_bool($v))
-            {
-                return false;
-            }
-            if(is_float($v))
-            {
-                return 0.0;
-            }
-            $class = get_class($v);
-            if($class !== false)
-            {
-                return new $class();
-            }
-            throw new Exception("Unable to find a suitable empty value for the type of 'v'");
-        });
-        return $emptied(...$args);
+        if(is_array($v))
+        {
+            return [];
+        }
+        if(is_iterable($v))
+        {
+            $fn = function() {
+                yield from [];
+            };
+            $init = self::generatorToIterable($fn);
+            return $init;
+        }
+        if(is_string($v))
+        {
+            return "";
+        }
+        if(is_int($v))
+        {
+            return 0;
+        }
+        if(is_bool($v))
+        {
+            return false;
+        }
+        if(is_float($v))
+        {
+            return 0.0;
+        }
+        $class = get_class($v);
+        if($class !== false)
+        {
+            return new $class();
+        }
+        throw new Exception("Unable to find a suitable empty value for the type of 'v'");
     }
 }
