32102a6294910c8ae02dc99995fb342f3c1d1eae
Matt <MattTaylor012333@git.hub>
2022-09-03 11:38:33 +0100

    Removed currying from "values".

--- src/collection/Values.php
+++ src/collection/Values.php
@@ -8,42 +8,41 @@ namespace FPHP\collection;
 
 trait Values
 {
-    public static function valuesT(...$args)
+    public static function valuesT(callable $step)
     {
-        $valuesT = self::curry(function(callable $step) {
-            return function($acc, $v) use($step) {
-                return $step($acc, $v);
-            };
-        });
-        return $valuesT(...$args);
+        return function($acc, $v) use($step) {
+            return $step($acc, $v);
+        };
     }
 
-    public static function values(...$args)
+    public static function values($target)
     {
-        $values = self::curry(function($target) {
-            $transduceInto = self::transduce(self::valuesT(), fn($acc, $v) => self::append($acc, $v), self::__(), $target);
-            if(is_object($target) && method_exists($target, "values"))
-            {
-                $out = $target->values();
-            }
-            else if(is_array($target))
-            {
-                $out = array_values($target);
-            }
-            else if(is_iterable($target))
-            {
-                $out = $transduceInto(self::emptied($target));
-            }
-            else if(is_object($target))
-            {
-                $out = $transduceInto(self::emptied([]));
-            }
-            else
-            {
-                throw new InvalidArgumentException("'target' must be iterable or object");
-            }
-            return $out;
-        });
-        return $values(...$args);
+        $transduceInto = fn($initial, $target) => self::transduce(
+            fn($step) => self::valuesT($step),
+            fn($acc, $v) => self::append($acc, $v),
+            $initial,
+            $target
+        );
+        if(is_object($target) && method_exists($target, "values"))
+        {
+            $out = $target->values();
+        }
+        else if(is_array($target))
+        {
+            $out = array_values($target);
+        }
+        else if(is_iterable($target))
+        {
+            $out = $transduceInto(self::emptied($target), $target);
+        }
+        else if(is_object($target))
+        {
+            $out = $transduceInto(self::emptied([]), $target);
+        }
+        else
+        {
+            throw new InvalidArgumentException("'target' must be iterable or object");
+        }
+        return $out;
     }
 }
