cbed342bea3dfbb9935cd84d3510e40f508fe3b8
Matt <MattTaylor012333@git.hub>
2022-11-19 16:52:45 +0000

    Added comments to "adjust" and "adjustT"

--- bin/FPHP.php
+++ bin/FPHP.php
@@ -19,20 +19,34 @@ use Traversable;
 
 final class FPHP
 {
-    public static function adjustT($idx, callable $transform, callable $step)
+    /**
+     * Creates a transducer function.
+     *
+     * Every value passed into the transducer function is passed to the step
+     * function, except that the value at the given index is transformed using
+     * the given transformation function before being passed to the step function.
+     *
+     * @param mixed $idx            index
+     * @param callable $transform   transformation function
+     * @param callable $step        step function
+     *
+     * @return callable transducer
+     */
+    public static function adjustT(mixed $idx, callable $transform, callable $step) : callable
     {
         return fn($acc, $v, $k) => $step($acc, $k === $idx ? $transform($v, $k) : $v, $k);
     }
 
-    public static function adjust($idx, callable $transform, $list) {
+    public static function adjust(mixed $idx, callable $transform, mixed $collection) : mixed
+    {
         return self::transduce(
             fn($step) => self::adjustT($idx, $transform, $step),
             // always use "assoc" for step function as we can't tell if a traversable is
             // associative or not without iterating it, and we can't do that in case it
             // is infinite. Adjust preserves keys anyway, so using assoc is fine.
             fn($acc, $v, $k) => self::assoc($acc, $v, $k),
-            self::emptied($list),
-            $list
+            self::emptied($collection),
+            $collection
         );
     }
 
