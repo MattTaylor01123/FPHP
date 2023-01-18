c3a8e1ee2ce4a946f19f022e95eb134b8556b5ff
Matt <MattTaylor012333@git.hub>
2023-01-18 22:17:19 +0000

    Added "nth" and "nth" test cases.

--- bin/FPHP.php
+++ bin/FPHP.php
@@ -889,40 +889,51 @@ final class FPHP
         return $out;
     }
 
-    public static function nth(...$args)
+    /**
+     * Returns the nth element of a collection.
+     *
+     * if $n is positive then the $nth value is returned.
+     * if $n is negative then the length + $nth value is returned.
+     * if $n >= length or length + $n < 0 then null is returned
+     *
+     * @param int $n                index of element in collection to return
+     * @param iterable $target      collection to look in
+     *
+     * @return mixed    the element at the nth position
+     */
+    public static function nth(int $n, iterable $target)
     {
-        $nth = self::curry(function(int $n, $target) {
-            $isArr = is_array($target);
-            if($n >= 0 && !$isArr)
+        $isArr = is_array($target);
+        if($n >= 0 && !$isArr)
+        {
+            $acc = 0;
+            $out = null;
+            foreach($target as $v)
             {
-                $acc = 0;
-                $out = null;
-                foreach($target as $v)
+                if($acc === $n)
                 {
-                    if($acc === $n)
-                    {
-                        $out = $v;
-                        break;
-                    }
+                    $out = $v;
+                    break;
                 }
+                $acc++;
+            }
+        }
+        else
+        {
+            $vals = $isArr ? $target : iterator_to_array($target, false);
+            $len = count($vals);
+            if($len === 0 || $n >= $len || $len + $n < 0)
+            {
+                $out = null;
             }
             else
             {
-                $vals = $isArr ? $target : iterator_to_array($target, false);
-                $len = count($vals);
-                if($len === 0 || $n > $len || $len + $n < 0)
-                {
-                    $out = null;
-                }
-                else
-                {
-                    $idx = ($n > 0 ? $n : $len + $n);
-                    $key = array_keys($vals)[$idx];
-                    $out = $vals[$key];
-                }
+                $idx = ($n >= 0 ? $n : $len + $n);
+                $key = array_keys($vals)[$idx];
+                $out = $vals[$key];
             }
-            return $out;
-        });
+        }
+        return $out;
     }
 
     public static function partitionByT(callable $fnGroup, callable $step)
---
+++ src/collection/Nth.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * (c) Matthew Taylor
+ */
+
+namespace src\collection;
+
+trait Nth
+{
+    /**
+     * Returns the nth element of a collection.
+     *
+     * if $n is positive then the $nth value is returned.
+     * if $n is negative then the length + $nth value is returned.
+     * if $n >= length or length + $n < 0 then null is returned
+     *
+     * @param int $n                index of element in collection to return
+     * @param iterable $target      collection to look in
+     *
+     * @return mixed    the element at the nth position
+     */
+    public static function nth(int $n, iterable $target)
+    {
+        $isArr = is_array($target);
+        if($n >= 0 && !$isArr)
+        {
+            $acc = 0;
+            $out = null;
+            foreach($target as $v)
+            {
+                if($acc === $n)
+                {
+                    $out = $v;
+                    break;
+                }
+                $acc++;
+            }
+        }
+        else
+        {
+            $vals = $isArr ? $target : iterator_to_array($target, false);
+            $len = count($vals);
+            if($len === 0 || $n >= $len || $len + $n < 0)
+            {
+                $out = null;
+            }
+            else
+            {
+                $idx = ($n >= 0 ? $n : $len + $n);
+                $key = array_keys($vals)[$idx];
+                $out = $vals[$key];
+            }
+        }
+        return $out;
+    }
+}
