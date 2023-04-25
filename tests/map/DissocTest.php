298ffa9775a8b0dbcfda998344821a47b1710e55
Matt <MattTaylor012333@git.hub>
2023-04-25 22:12:45 +0100

    Added custom type test for "dissoc".

--- tests/map/DissocTest.php
+++ tests/map/DissocTest.php
@@ -8,6 +8,7 @@ namespace tests\map;
 
 use FPHP\FPHP as F;
 use PHPUnit\Framework\TestCase;
+use tests\TestType;
 
 final class DissocTest extends TestCase
 {
@@ -23,5 +24,18 @@ final class DissocTest extends TestCase
         $obj = ["name" => "Matt", "age" => 47];
         $out = F::dissoc($obj, "age");
         $this->assertEquals(["name" => "Matt"], $out);        
+    }
+    
+    public function testAssocCusType()
+    {
+        $obj = new TestType();
+        $obj->a = 5;
+        $obj->b = "h";
+        $obj2 = F::dissoc($obj, "b");
+
+        $exp = new TestType();
+        $exp->a = 5;
+        $this->assertTrue($obj2 instanceof TestType);
+        $this->assertEquals($exp, $obj2);
     }
 }
