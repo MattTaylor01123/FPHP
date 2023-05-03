b6896ee378dbba6343995a648875d66b57d21e30
Matt <MattTaylor012333@git.hub>
2023-05-03 21:49:49 +0100

    Moved "merge" to the map folder.

--- tests/map/MergeLeftTest.php
+++ tests/map/MergeLeftTest.php
@@ -4,28 +4,28 @@
  * (c) Matthew Taylor
  */
 
-namespace tests\collection;
+namespace tests\map;
 
 use FPHP\FPHP as F;
 use PHPUnit\Framework\TestCase;
 
-final class MergeRightTest extends TestCase
+final class MergeLeftTest extends TestCase
 {
     public function testMerge()
     {
-        $out1 = F::mergeRight(["a" => 1, "b" => 2], ["a" => 3, "c" => 4]);
-        $this->assertEquals(["a" => 3, "b" => 2, "c" => 4], $out1);
+        $out1 = F::mergeLeft(["a" => 1, "b" => 2], ["a" => 3, "c" => 4]);
+        $this->assertEquals(["a" => 1, "b" => 2, "c" => 4], $out1);
 
-        $out2 = F::mergeRight((object)["a" => 1, "b" => 2],(object)["a" => 3, "c" => 4]);
-        $this->assertEquals((object)["a" => 3, "b" => 2, "c" => 4], $out2);
+        $out2 = F::mergeLeft((object)["a" => 1, "b" => 2],(object)["a" => 3, "c" => 4]);
+        $this->assertEquals((object)["a" => 1, "b" => 2, "c" => 4], $out2);
     }
     
     public function testMixedInputs()
     {
-        $out1 = F::mergeRight(["a" => 1, "b" => 2, "c" => 1], (object)["c" => 3, "d" => "dog"]);
+        $out1 = F::mergeLeft((object)["c" => 3, "d" => "dog"], ["a" => 1, "b" => 2, "c" => 1]);
         $this->assertEquals(["a" => 1, "b" => 2, "c" => 3, "d" => "dog"], $out1);
         
-        $out2 = F::mergeRight((object)["cat" => 7, "dog" => 3], ["mouse" => 5, "cat" => 1]);
+        $out2 = F::mergeLeft(["mouse" => 5, "cat" => 1], (object)["cat" => 7, "dog" => 3]);
         $this->assertEquals((object)["cat" => 1, "dog" => 3, "mouse" => 5], $out2);
     }
     
@@ -33,14 +33,14 @@ final class MergeRightTest extends TestCase
     {
         $obj = new class() {
             public $params = null;
-            public function mergeRight(...$args)
+            public function mergeLeft(...$args)
             {
                 $this->params = $args;
                 return ["hello" => "world"];
             }
         };
         
-        $out = F::mergeRight($obj, ["a" => 1, "b" => 2]);
+        $out = F::mergeLeft(["a" => 1, "b" => 2], $obj);
         $this->assertEquals([["a" => 1, "b" => 2]], $obj->params);
         $this->assertEquals(["hello" => "world"], $out);
     }
--- tests/map/MergeRightTest.php
+++ tests/map/MergeRightTest.php
@@ -4,28 +4,28 @@
  * (c) Matthew Taylor
  */
 
-namespace tests\collection;
+namespace tests\map;
 
 use FPHP\FPHP as F;
 use PHPUnit\Framework\TestCase;
 
-final class MergeLeftTest extends TestCase
+final class MergeRightTest extends TestCase
 {
     public function testMerge()
     {
-        $out1 = F::mergeLeft(["a" => 1, "b" => 2], ["a" => 3, "c" => 4]);
-        $this->assertEquals(["a" => 1, "b" => 2, "c" => 4], $out1);
+        $out1 = F::mergeRight(["a" => 1, "b" => 2], ["a" => 3, "c" => 4]);
+        $this->assertEquals(["a" => 3, "b" => 2, "c" => 4], $out1);
 
-        $out2 = F::mergeLeft((object)["a" => 1, "b" => 2],(object)["a" => 3, "c" => 4]);
-        $this->assertEquals((object)["a" => 1, "b" => 2, "c" => 4], $out2);
+        $out2 = F::mergeRight((object)["a" => 1, "b" => 2],(object)["a" => 3, "c" => 4]);
+        $this->assertEquals((object)["a" => 3, "b" => 2, "c" => 4], $out2);
     }
     
     public function testMixedInputs()
     {
-        $out1 = F::mergeLeft((object)["c" => 3, "d" => "dog"], ["a" => 1, "b" => 2, "c" => 1]);
+        $out1 = F::mergeRight(["a" => 1, "b" => 2, "c" => 1], (object)["c" => 3, "d" => "dog"]);
         $this->assertEquals(["a" => 1, "b" => 2, "c" => 3, "d" => "dog"], $out1);
         
-        $out2 = F::mergeLeft(["mouse" => 5, "cat" => 1], (object)["cat" => 7, "dog" => 3]);
+        $out2 = F::mergeRight((object)["cat" => 7, "dog" => 3], ["mouse" => 5, "cat" => 1]);
         $this->assertEquals((object)["cat" => 1, "dog" => 3, "mouse" => 5], $out2);
     }
     
@@ -33,14 +33,14 @@ final class MergeLeftTest extends TestCase
     {
         $obj = new class() {
             public $params = null;
-            public function mergeLeft(...$args)
+            public function mergeRight(...$args)
             {
                 $this->params = $args;
                 return ["hello" => "world"];
             }
         };
         
-        $out = F::mergeLeft(["a" => 1, "b" => 2], $obj);
+        $out = F::mergeRight($obj, ["a" => 1, "b" => 2]);
         $this->assertEquals([["a" => 1, "b" => 2]], $obj->params);
         $this->assertEquals(["hello" => "world"], $out);
     }
